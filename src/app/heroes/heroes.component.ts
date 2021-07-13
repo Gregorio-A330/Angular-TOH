be36a11f8ae03aacc355863856f7490db3e60dcc
Gabriel Gregorio <gabriel@stymeapp.com>
2021-07-13 16:25:42 -0300

    Etapa 4 - add services - incluindo mensagens, funções e binds em serviços

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,19 +10,28 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-  heroes = HEROES;
-
-  constructor() { }
-
-  ngOnInit() {
-  }
 
   selectedHero?: Hero;
-  onSelect(hero: Hero): void {
+
+  heroes: Hero[] = [];
+// criando um serviço de heroi privado
+constructor(private heroService: HeroService, private messageService: MessageService) { }
+//criando a função GET dos herois e dando "bind" ao array criado acima
+  getHeroes(): void {
+    //quando for realizar uma chamada desse padrão e vier de um servidor, sempre utilizar uma função assincrona
+    //para que tenha o devido efeito
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);//utilizando este metodo, pois não estamos consumindo de um servidor remoto
+  }
+//ao iniciar ele chama a função get e plota dentro do array
+  ngOnInit() {
+    this.getHeroes();
+  }
+// caso selecione um heroi ele seta o heroi selecionado que vem de "hero"
+
+onSelect(hero: Hero): void {
   this.selectedHero = hero;
+  this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
 }
+
 }
